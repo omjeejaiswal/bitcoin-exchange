2bdeab4478956554e02d00ba246ef9fb1c4f4f2d
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-08-29 12:23:56 +0530

    .

---
+++ frontend/app/componets/Hero.tsx
@@ -0,0 +1,19 @@
+
+
+export const Hero = () => {
+    return <div>
+        <div className="text-5xl font-medium">
+            <span>
+                The Indian Cryptocurrency Revolution
+            </span>
+
+            <span className="text-blue-500 pl-4 ">
+                Revolution
+            </span>
+        </div>
+        <div className="flex justify-center pt-4">
+            Create a frictionless wallet with just a Google Account.
+            Convert your INR into Cryptocurrency
+        </div>
+    </div>
+}
